1105093c06ec51de24101cf62f53f65dce1e453d
TheNeo <161718261+TheDye@users.noreply.github.com>
2024-04-08 22:09:28 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-// LOCAL DATAS ASVAVSAVSs
-const users = [
-  { username: 'admin', password: 'buynaa0401' },
-  { username: 'irmuun', password: 'oros320228b' },
-  { username: 'temka', password: 'oros320228b' },
-  { username: 'monhtulga', password: 'oros320228b' },
-  { username: 'tselmeg', password: 'oros320228b' },
-  { username: '8b', password: '8b' },
-  { username: 'test', password: 'test' },
-  // Add more users as needed
-];
+// CHECK LOGIN FUNCTION
+function checkLogin(event) {
+  event.preventDefault();
 
-function checkLogin() {
   const enteredUsername = document.getElementById('username').value;
   const enteredPassword = document.getElementById('password').value;
 
-  // CORRECT OR NO NIGGA
+  // 
+  const users = [
+    { username: 'test', password: 'test' },
+  ];
+
+  // CHECK IF LOGIN IS VALID
   const isValidLogin = users.some(user => user.username === enteredUsername && user.password === enteredPassword);
 
   if (isValidLogin) {
     alert('Login successful! Redirecting to another page...');
-    // MOVE TO DIFFERENT PLANET
     window.location.href = "Homepage/Home.html";
   } else {
     alert('Invalid username or password. Please try again.');
   }
 }
+
+// ADD EVENT LISTENER
+document.getElementById('loginForm').addEventListener('submit', checkLogin);
